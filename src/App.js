a15b2f9cb6ae7170927ff72760849c08e20a895a
Vivekanand Bharkad <bharkadvivek@gmail.com>
2024-01-14 20:51:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Sidebar from "./sidebar";
+import Navbar from "./navbar";
+import Dashboard from "./dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Table from "./table";
+import Createuser from "./foms";
+import UserInfo from "./userInfo";
+import Login from "./loginpage";
+import Useredit from "./Useredit";
+import Teachers from "./teachers";
+import { UserProvider } from "./usercontext";
+import Studentregister from "./studentregister";
+import Addstudent from "./addstudent";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+<UserProvider>
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path='/Dashboard' element={<Dashboard></Dashboard>}/> 
+                <Route path="/Dashboard/teachers" element={<Table />} />
+                <Route path="/Dashboard/teachers/students/:id" element={<Table />} />
+                <Route path="/Dashboard/tables/createuser" element={<Createuser/>}/>
+                <Route path="/Dashboard/tables/studentregister" element={<Studentregister/>}/>
+                <Route path="/Dashboard/tables/view/:id" element={<UserInfo></UserInfo>}/>
+                <Route path="/Dashboard/tables/edit/:id" element={<Useredit></Useredit>}/>
+                <Route path = "/Dashboard/tables/:id/addstudents" element = {<Addstudent/>}/>
+                <Route path="/Dashboard/students" element={<Teachers></Teachers>}/>
+              </Routes>
+              </UserProvider>
+      
+    </BrowserRouter>
   );
 }
 
